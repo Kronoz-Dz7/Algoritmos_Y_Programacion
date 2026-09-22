e37a560dc610aecdbd3480bfc52b2c1ee0b01d84
Daniel Álvarez <danielalvarezt16@gmail.com>
2026-09-22 11:15:25 -0500

    Create Imprimir.cs to print numbers 1 to 100
    Implement a program that prints numbers from 1 to 100.

---
+++ Imprimir.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imprimir
+{
+    //Utiliza un ciclo for para mostrar los números del 1 al 100
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            { Console.WriteLine();
+                Console.Write(i);
+            }
